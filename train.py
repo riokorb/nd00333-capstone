0c5923bc8c2ffef7511c47ce2c58ac569ae4dba8
riokorb <72217058+riokorb@users.noreply.github.com>
2021-01-04 15:55:04 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -4,10 +4,11 @@ import os
 import numpy as np
 import pandas as pd
 import joblib
-from sklearn.metrics import mean_squared_error, f1_score, precision_score, recall_score, auc
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from azureml.core.run import Run, Dataset
+from azureml.core.run import Run
 from azureml.core.workspace import Workspace
+from azureml.data.dataset_factory import TabularDatasetFactory
 
 run = Run.get_context()
 
@@ -17,16 +18,16 @@ def clean_data(data):
     x_df.drop("CLIENTNUM", inplace=True, axis=1)
     gender = pd.get_dummies(x_df.Gender, prefix="Gender")
     x_df.drop("Gender", inplace=True, axis=1)
-    education_level = pd.get_dummies(x_df.education_level, prefix="Education_Level")
+    education_level = pd.get_dummies(x_df.Education_Level, prefix="Education_Level")
     x_df.drop("Education_Level", inplace=True, axis=1)
     x_df = x_df.join(education_level)
-    marital_status = pd.get_dummies(x_df.marital_status, prefix="Marital_Status")
+    marital_status = pd.get_dummies(x_df.Marital_Status, prefix="Marital_Status")
     x_df.drop("Marital_Status", inplace=True, axis=1)
     x_df = x_df.join(marital_status)
-    income_category = pd.get_dummies(x_df.income_category, prefix="Income_Category")
+    income_category = pd.get_dummies(x_df.Income_Category, prefix="Income_Category")
     x_df.drop("Income_Category", inplace=True, axis=1)
     x_df = x_df.join(income_category)
-    card_category = pd.get_dummies(x_df.card_category, prefix="Card_Category")
+    card_category = pd.get_dummies(x_df.Card_Category, prefix="Card_Category")
     x_df.drop("Card_Category", inplace=True, axis=1)
     x_df = x_df.join(card_category)
     y_df = x_df.pop("Attrition_Flag").apply(lambda s: 1 if s == "Attrited Customer" else 0)
@@ -34,44 +35,46 @@ def clean_data(data):
     return x_df, y_df
 
 def retrieve_cleaned_data():
-    ws = run.experiment.workspace
-    ds_name = 'bankchurners'
-    ds = Dataset.get_by_name(workspace = ws, name = ds_name)
+    # Create TabularDataset using TabularDatasetFactory
+    url = "https://raw.githubusercontent.com/riokorb/nd00333-capstone/master/BankChurners.csv"
+    ds = TabularDatasetFactory.from_delimited_files(path=url)
+    # clean dataset
     x, y = clean_data(ds)
     return train_test_split(x, y, test_size = 0.3, random_state = 0)
+
+run = Run.get_context()
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_estimators', type=int, default=100, help="Number of trees in the forest")
-    parser.add_argument('--max_depth', type=int, default=None, help="The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.")
-    parser.add_argument('--min_samples_split', type=int, default=2, help="The minimum number of samples required to split an internal node.")
-    parser.add_argument('--min_samples_leaf', type=int, default=1, help="The minimum number of samples required to be at a leaf node.")
+    parser.add_argument('--kernel', type=str, default='linear',
+                        help='Kernel type to be used in the algorithm')
+    parser.add_argument('--penalty', type=float, default=1.0,
+                        help='Penalty parameter of the error term')
 
     args = parser.parse_args()
-
-    if args.max_depth == 0:
-        max_depth = None
-    else:
-        max_depth = args.max_depth
-
-    run.log("Num Estimators:", np.float(args.n_estimators))
-    run.log("Max Depth:", max_depth)
-    run.log("Min Samples Split:", np.int(args.min_samples_split))
-    run.log("Min Samples Leaf:", np.int(args.min_samples_leaf))
+    run.log('Kernel type', np.str(args.kernel))
+    run.log('Penalty', np.float(args.penalty))
 
     x_train, x_test, y_train, y_test = retrieve_cleaned_data()
 
-    model = RandomForestClassifier(n_estimators=args.n_estimators,max_depth=max_depth,min_samples_split=args.min_samples_split,min_samples_leaf=args.min_samples_leaf).fit(x_train, y_train)
-    accuracy = model.score(x_test, y_test)
-    f1 = f1_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+    # training a linear SVM classifier
+    from sklearn.svm import SVC
+    svm_model_linear = SVC(kernel=args.kernel, C=args.penalty).fit(x_train, y_train)
+    svm_predictions = svm_model_linear.predict(x_test)
 
-    run.log("Accuracy", np.float(accuracy))
-    run.log("F1 Score", np.float(f1))
-    run.log("Precision", np.float(precision))
-    run.log("Recall", np.float(recall))
+    # model accuracy for X_test
+    accuracy = svm_model_linear.score(x_test, y_test)
+    print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
+    run.log('Accuracy', np.float(accuracy))
+    # creating a confusion matrix
+    cm = confusion_matrix(y_test, svm_predictions)
+    print(cm)
+
+    os.makedirs('outputs', exist_ok=True)
+    # files saved in the "outputs" folder are automatically uploaded into run history
+    joblib.dump(svm_model_linear, 'outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
